05e42847c5f8f3337a242ab6541b8d027ca186c5
Ziauddin <md.ziauddin02@gmail.com>
2021-02-07 12:48:57 +0530

    code Refactoring, mongoose middleware & validation

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -6,9 +6,12 @@ import {
   createTour,
   updateTour,
   deleteTour,
+  aliasTopTours,
 } from '../controllers/tourController.js';
 
 export const tourRouter = express.Router();
+
+tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
